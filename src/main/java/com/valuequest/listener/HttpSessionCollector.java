2504d1b994886550ab18efca2ceb57ccfbc04956
FDSAP-Adequilla <alex.dequilla@fortress-asya.com>
2023-07-28 09:46:05 +0800

    nothing commit

--- src/main/java/com/valuequest/listener/HttpSessionCollector.java
+++ src/main/java/com/valuequest/listener/HttpSessionCollector.java
@@ -1,29 +1,47 @@
 package com.valuequest.listener;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 public class HttpSessionCollector implements HttpSessionListener {
-    private static final Map<String, HttpSession> sessions = new HashMap<String, HttpSession>();
+
+    private static final ConcurrentHashMap<String, HttpSession> sessions = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, Integer> userSessionCounts = new ConcurrentHashMap<>();
+    private static final int MAX_CONCURRENT_SESSIONS = 1; // Maximum allowed concurrent sessions per user
 
     @Override
     public void sessionCreated(HttpSessionEvent event) {
         HttpSession session = event.getSession();
         sessions.put(session.getId(), session);
-    }
 
+        // Update the user session count
+        String username = (String) session.getAttribute("username"); // Replace "username" with the actual attribute name you use to store the username in the session
+        if (username != null) {
+            userSessionCounts.put(username, userSessionCounts.getOrDefault(username, 0) + 1);
+
+            // Check if the user has exceeded the maximum allowed concurrent sessions
+            if (userSessionCounts.get(username) > MAX_CONCURRENT_SESSIONS) {
+                // Invalidate the newly created session as it exceeds the limit
+                session.invalidate();
+            }
+        }
+    }
 
     @Override
     public void sessionDestroyed(HttpSessionEvent event) {
-        sessions.remove(event.getSession().getId());
+        HttpSession session = event.getSession();
+        sessions.remove(session.getId());
+
+        // Update the user session count when a session is destroyed
+        String username = (String) session.getAttribute("username"); // Replace "username" with the actual attribute name you use to store the username in the session
+        if (username != null) {
+            userSessionCounts.put(username, userSessionCounts.getOrDefault(username, 1) - 1);
+        }
     }
 
     public static HttpSession find(String sessionId) {
         return sessions.get(sessionId);
     }
-
 }
--- src/main/java/com/valuequest/listener/LoginAuthenticationProvider.java
+++ src/main/java/com/valuequest/listener/LoginAuthenticationProvider.java
@@ -38,132 +38,133 @@ import com.valuequest.entity.security.SecUser;
 import com.valuequest.services.AdminService;
 import com.valuequest.services.GenericService;
 
+
 @Component("authenticationProvider")
-public class LoginAuthenticationProvider implements AuthenticationProvider{
+public class LoginAuthenticationProvider implements AuthenticationProvider {
 
-	@Autowired
-	protected AdminService adminService;
-	
-	@Autowired
-	protected GenericService genericService;
-	
-	@Override
-	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		
-		String username = authentication.getName();
+    @Autowired
+    protected AdminService adminService;
+
+    @Autowired
+    protected GenericService genericService;
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = authentication.getName();
         String password = (String) authentication.getCredentials();
-        SecUser user 	= adminService.getSecUser(username);
-		// StructureUser userId = (String) authentication.getUserId();
-        
+        SecUser user = adminService.getSecUser(username);
+
         if (user == null) {
-			throw new BadCredentialsException("User does not exist.");
-		}else{
-			// if(userId == userId){
-			// 	throw new BadCredentialsException("User already login.");
-			// }
-			
-			String passwordEncode = adminService.encodePassword(password);
-			if(StringUtils.equals(passwordEncode, user.getUsrPassword())){
+            throw new BadCredentialsException("User does not exist.");
+        } else {
 
-			// 	if (user.getSessionId() != null && !user.getSessionId().equals(session.getId())) {
-            //     response.getWriter().println("User is already logged in from another session.");
-            //     return;
-            // }
-			//
+            String passwordEncode = adminService.encodePassword(password);
+            if (StringUtils.equals(passwordEncode, user.getUsrPassword())) {
 
-				// if (user.getId() == (user.getId())){
-				// 	throw new BadCredentialsException("User already login.");
-				// }
-				if (HttpSessionCollector.find(user.getCheckStatus()) != null){
-					 ParamConfig config = genericService.getConfigByName(ParamConfig.SESSION_TIMEOUT_WEBTOOL);
-			         String valueTimeout = config.getValue();
-			         valueTimeout = valueTimeout.replace(".","");
-			         valueTimeout = valueTimeout.replace(",","");
-					throw new DisabledException("Your account is being used/locked. Please re-login again after "+valueTimeout+" minutes or contact your Web Application Administrator to unlock your account.");
-				}
-				else {
-					System.out.println("##################### user? "+ user);
-					if (Lookup.LOOKUP_USER_STATUS_LOCK.equals(user.getUsrStatus())){
-						throw new DisabledException("Your Account has been Blocked. Please Contact Customer Service.");
-						
-					}else if (Lookup.LOOKUP_USER_STATUS_INACTIVE.equals(user.getUsrStatus())){
-						throw new DisabledException("Your Account is Inactive. Please Contact Customer Service.");
-						
-					}else{
-						Date activationLimit = user.getActivationLimit();
-						if(activationLimit != null){
-							Calendar cal1 = Calendar.getInstance();
-							cal1.setTime(activationLimit);
-							Calendar cal2 = Calendar.getInstance();
-							cal2.setTime(new Date());
-							
-							if(user.isPasswordDefault() && cal1.compareTo(cal2) < 0){
-								throw new DisabledException("Your Password is expired. Please Contact Customer Service.");
-							}
-						}
-	
-						adminService.resetFailAttempts(user);
-						Collection<? extends GrantedAuthority> authorities = getGrantedAuthority(user);
-						return new UsernamePasswordAuthenticationToken(user, password, authorities);
-					}
-				}
-			}else{
-				int MAX_LOGIN_ATTEMPTS = 3;
-				ParamConfig config	= genericService.getConfigByName(ParamConfig.WEB_INVALID_PASSWD_LIMIT);
-				
-				if( config != null ){
-					MAX_LOGIN_ATTEMPTS	= Integer.valueOf(config.getValue());
-				}
-				
-				adminService.updateFailAttempts(user, MAX_LOGIN_ATTEMPTS);
-				
-				if(user.getLoginAttempts() != null && user.getLoginAttempts() >= MAX_LOGIN_ATTEMPTS){
-					throw new BadCredentialsException("Your account has been blocked due to " + MAX_LOGIN_ATTEMPTS + " or more wrong input of password, please request password reset in order to login your account.");
-					
-				}else{
-					throw new BadCredentialsException("Please Input correct username and password");
-					
-				}
-			}
-		}
-	}
-	
-	@Override
-	public boolean supports(Class<?> authentication) {
-		return authentication.equals(UsernamePasswordAuthenticationToken.class);
-	}
+                HttpSession existingSession = HttpSessionCollector.find(username);
+                if (existingSession != null && !existingSession.getId().equals(getCurrentSessionId())) {
+                    // Existing session found, invalidate the previous session
+                    existingSession.invalidate();
+                }
 
-	private Collection<GrantedAuthority> getGrantedAuthority(SecUser user) {
+                if (HttpSessionCollector.find(username) != null) {
+                    ParamConfig config = genericService.getConfigByName(ParamConfig.SESSION_TIMEOUT_WEBTOOL);
+                    String valueTimeout = config.getValue();
+                    valueTimeout = valueTimeout.replace(".", "");
+                    valueTimeout = valueTimeout.replace(",", "");
+                    throw new DisabledException(
+                            "Your account is being used/locked. Please re-login again after " + valueTimeout
+                                    + " minutes or contact your Web Application Administrator to unlock your account.");
+                } else {
+                    System.out.println("##################### user? " + user);
+                    if (Lookup.LOOKUP_USER_STATUS_LOCK.equals(user.getUsrStatus())) {
+                        throw new DisabledException("Your Account has been Blocked. Please Contact Customer Service.");
 
-		Collection<String> rights = null;
-		if(user.getUsrPosition() != null && user.getUsrPosition().equalsIgnoreCase(SecUser.USER_SUPER_ADMIN)){
-			rights = adminService.getAllRights();
-		}else{
-			rights = adminService.getRightsByUser(user);
-		}
-		
-		ArrayList<GrantedAuthority> rechteGrantedAuthorities = new ArrayList<GrantedAuthority>(rights.size());
+                    } else if (Lookup.LOOKUP_USER_STATUS_INACTIVE.equals(user.getUsrStatus())) {
+                        throw new DisabledException("Your Account is Inactive. Please Contact Customer Service.");
 
-		for (String right : rights) {
-			rechteGrantedAuthorities.add(new GrantedAuthorityImpl(right));
-			
-		}
-		
-		return rechteGrantedAuthorities;
-	}
-	
-	class GrantedAuthorityImpl implements GrantedAuthority {
-		
-		private static final long serialVersionUID = 1L;
-		
-		private String role;
-	 
-	    public GrantedAuthorityImpl(String role) {
-	        this.role = role;
-	    }
-	 
-	    public String getAuthority() {
-	        return role;
-	    }
-	}
+                    } else {
+                        Date activationLimit = user.getActivationLimit();
+                        if (activationLimit != null) {
+                            Calendar cal1 = Calendar.getInstance();
+                            cal1.setTime(activationLimit);
+                            Calendar cal2 = Calendar.getInstance();
+                            cal2.setTime(new Date());
+
+                            if (user.isPasswordDefault() && cal1.compareTo(cal2) < 0) {
+                                throw new DisabledException("Your Password is expired. Please Contact Customer Service.");
+                            }
+                        }
+
+                        adminService.resetFailAttempts(user);
+                        Collection<? extends GrantedAuthority> authorities = getGrantedAuthority(user);
+                        return new UsernamePasswordAuthenticationToken(user, password, authorities);
+                    }
+                }
+            } else {
+                int MAX_LOGIN_ATTEMPTS = 3;
+                ParamConfig config = genericService.getConfigByName(ParamConfig.WEB_INVALID_PASSWD_LIMIT);
+
+                if (config != null) {
+                    MAX_LOGIN_ATTEMPTS = Integer.valueOf(config.getValue());
+                }
+
+                adminService.updateFailAttempts(user, MAX_LOGIN_ATTEMPTS);
+
+                if (user.getLoginAttempts() != null && user.getLoginAttempts() >= MAX_LOGIN_ATTEMPTS) {
+                    throw new BadCredentialsException("Your account has been blocked due to " + MAX_LOGIN_ATTEMPTS
+                            + " or more wrong input of password, please request password reset in order to login your account.");
+
+                } else {
+                    throw new BadCredentialsException("Please Input correct username and password");
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+    private Collection<GrantedAuthority> getGrantedAuthority(SecUser user) {
+        Collection<String> rights = null;
+        if (user.getUsrPosition() != null && user.getUsrPosition().equalsIgnoreCase(SecUser.USER_SUPER_ADMIN)) {
+            rights = adminService.getAllRights();
+        } else {
+            rights = adminService.getRightsByUser(user);
+        }
+
+        ArrayList<GrantedAuthority> rechteGrantedAuthorities = new ArrayList<GrantedAuthority>(rights.size());
+
+        for (String right : rights) {
+            rechteGrantedAuthorities.add(new GrantedAuthorityImpl(right));
+        }
+
+        return rechteGrantedAuthorities;
+    }
+
+    class GrantedAuthorityImpl implements GrantedAuthority {
+
+        private static final long serialVersionUID = 1L;
+
+        private String role;
+
+        public GrantedAuthorityImpl(String role) {
+            this.role = role;
+        }
+
+        public String getAuthority() {
+            return role;
+        }
+    }
+
+    private String getCurrentSessionId() {
+        HttpSession session = request.getSession(false);
+        return (session != null) ? session.getId() : null;
+    }
 }
